a68e210e9c6865fe79aac24188e8ba4bcb1a306a
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2018-12-22 16:35:22 -0500

    Set up jquery logic

--- game.js
+++ game.js
@@ -1,12 +1,27 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
+let gamePattern = [];
+let userClickedPattern = [];
+
+// Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+$('btn').click(function(){
+    let userChosenColor = $(this).attr('id');
+
+    userClickedPattern.push(userChosenColor);
+})
+
 function nextSequence() {
     let randomNumber = Math.round(Math.random() * 4);
-    
     //Create a new variable called randomChosenColor and use the randomNumber from step 2 to select a random color from the buttonColours array.
     let randomChosenColor = buttonColors[randomNumber]
-
     // Push it to gamePattern array
     gamePattern.push(randomChosenColor)
+
+    $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+    audio.play();
+    
 }
+
+
 
